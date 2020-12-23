efe9b1c2c40df51dca6393c6079b566493fad858
dhutsj <jszxtsj@163.com>
2020-12-23 15:45:45 +0800

    enhance

--- ls_cd_dir_grep_keyword.sh
+++ ls_cd_dir_grep_keyword.sh
@@ -2,7 +2,12 @@
 
 array=($(ls -rt /data/jenkins/jobs/))
 for val in ${array[@]}; do
-   cd /data/jenkins/jobs/$val
-   grep -irn "xxx.sh" ./config.xml
-   echo $val
+    if [ -d /data/jenkins/jobs/$val ]; then
+        cd /data/jenkins/jobs/$val
+        grep -irn "xxx.py" ./config.xml
+        if [ $? == 0 ]; then
+            echo $val
+        fi
+    fi
 done
+
